3d8b924287fa3c9ecabf05a79a2aa77233d32b31
gaurav-bhardwaj29 <gaurav290802@gmail.com>
2025-05-19 23:47:47 +0530

    header

--- src/server.cpp
+++ src/server.cpp
@@ -1,73 +1,66 @@
 #include <iostream>
 #include <cstring>
+#include <arpa/inet.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <unistd.h>
 
-int main() {
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+constexpr int PORT =2053;
+costexpr int BUFFER_SIZE =512;
 
-    // Disable output buffering
-    setbuf(stdout, NULL);
+int main(){
+    int sockfd;
+    struct sockaddr_in servr_addr{}, client_addr{};
+    socklen_t client_len = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here!" << std::endl;
-
-      // Uncomment this block to pass the first stage
-    int udpSocket;
-    struct sockaddr_in clientAddress;
-
-    udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (udpSocket == -1) {
-        std::cerr << "Socket creation failed: " << strerror(errno) << "..." << std::endl;
-        return 1;
+    // CREATE UDP SOCKET
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0){
+        perror("socket creation failed");
+        return -1;
     }
 
-    // Since the tester restarts your program quite often, setting REUSE_PORT
-    // ensures that we don't run into 'Address already in use' errors
-    int reuse = 1;
-    if (setsockopt(udpSocket, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-        std::cerr << "SO_REUSEPORT failed: " << strerror(errno) << std::endl;
-        return 1;
+    // Bind to port 2053
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_Addr)) <0){
+        perror("bind failed");
+        close(sockfd);
+        return -1;
     }
-
-    sockaddr_in serv_addr = { .sin_family = AF_INET,
-                              .sin_port = htons(2053),
-                              .sin_addr = { htonl(INADDR_ANY) },
-                            };
-
-    if (bind(udpSocket, reinterpret_cast<struct sockaddr*>(&serv_addr), sizeof(serv_addr)) != 0) {
-        std::cerr << "Bind failed: " << strerror(errno) << std::endl;
-        return 1;
-    }
-
-    int bytesRead;
-    char buffer[512];
-    socklen_t clientAddrLen = sizeof(clientAddress);
-
-    while (true) {
-        // Receive data
-        bytesRead = recvfrom(udpSocket, buffer, sizeof(buffer), 0, reinterpret_cast<struct sockaddr*>(&clientAddress), &clientAddrLen);
-        if (bytesRead == -1) {
-            perror("Error receiving data");
-            break;
+    while (true){
+        ssize_t n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,(struct sockaddr *)&client_addr, &client_len);
+        if(n<0){
+            perror("recvfrom failed");
+            continue;
         }
+        // BUILD DNS response header(12 bytes)
+        uint8_t response[12] = {};
 
-        buffer[bytesRead] = '\0';
-        std::cout << "Received " << bytesRead << " bytes: " << buffer << std::endl;
+        // Transaction ID: 1234 
+        response[0] = 0x04;
+        response[1] = 0x00;
 
-        // Create an empty response
-        char response[1] = { '\0' };
+        response[2] = 0x00;
+        response[3] = 0x00;
+        
+        response[4] = 0x00;
+        response[5] = 0x00;
 
-        // Send response
-        if (sendto(udpSocket, response, sizeof(response), 0, reinterpret_cast<struct sockaddr*>(&clientAddress), sizeof(clientAddress)) == -1) {
-            perror("Failed to send response");
-        }
+        response[6] = 0x00;
+        response[7] = 0x00;
+
+        response[8] = 0x00;
+        response[9] = 0x00;
+
+        response[10] = 0x00;
+        response[11] = 0x00;
+
+        sendto(sockfd, response, sizeof(response), 0, (struct sockaddr *)&client_addr, client_len);
+
     }
-
-    close(udpSocket);
-
+    close(sockfd);
     return 0;
 }
