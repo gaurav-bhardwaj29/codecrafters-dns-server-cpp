9b3fc1ac6c71d82b962eed6d44a301345a705faf
gaurav-bhardwaj29 <gaurav290802@gmail.com>
2025-05-20 00:05:41 +0530

    header

--- src/server.cpp
+++ src/server.cpp
@@ -4,20 +4,20 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-constexpr int PORT =2053;
-constexpr int BUFFER_SIZE =512;
+constexpr int PORT = 2053;
+constexpr int BUFFER_SIZE = 512;
 
-int main(){
+int main() {
     int sockfd;
     struct sockaddr_in server_addr{}, client_addr{};
     socklen_t client_len = sizeof(client_addr);
     char buffer[BUFFER_SIZE];
 
-    // CREATE UDP SOCKET
+    // Create UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sockfd < 0){
+    if (sockfd < 0) {
         perror("socket creation failed");
-        return -1;
+        return 1;
     }
 
     // Bind to port 2053
@@ -25,40 +25,50 @@ int main(){
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) <0){
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind failed");
         close(sockfd);
-        return -1;
+        return 1;
     }
-    while (true){
-        ssize_t n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,(struct sockaddr *)&client_addr, &client_len);
-        if(n<0){
+
+    while (true) {
+        ssize_t n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
+                             (struct sockaddr *)&client_addr, &client_len);
+        if (n < 0) {
             perror("recvfrom failed");
             continue;
         }
-        // BUILD DNS response header(12 bytes)
-        uint8_t response[BUFFER_SIZE] = {};
-        ssize_t offset = 0;
 
-        // Transaction ID: 1234 
+        // Build DNS response header (12 bytes)
+        uint8_t response[BUFFER_SIZE] = {};
+        size_t offset = 0;
+
+        // Transaction ID: 1234 (0x04D2)
         response[offset++] = 0x04;
         response[offset++] = 0xD2;
 
+        // Flags: 0x8000 (QR=1, rest 0)
         response[offset++] = 0x80;
         response[offset++] = 0x00;
-        
+
+        // QDCOUNT = 1
         response[offset++] = 0x00;
         response[offset++] = 0x01;
 
+        // ANCOUNT = 0
         response[offset++] = 0x00;
         response[offset++] = 0x00;
 
+        // NSCOUNT = 0
         response[offset++] = 0x00;
         response[offset++] = 0x00;
 
+        // ARCOUNT = 0
         response[offset++] = 0x00;
         response[offset++] = 0x00;
 
+        // Question Section
+        // Name: codecrafters.io => \x0ccodecrafters\x02io\x00
         const uint8_t qname[] = {
             0x0c, 'c','o','d','e','c','r','a','f','t','e','r','s',
             0x02, 'i','o',
@@ -83,5 +93,3 @@ int main(){
     close(sockfd);
     return 0;
 }
-        
-
